e4ec71528ed1ee2ba41dbe53651bb21b00ff5780
五十嵐海織 <miori@igarashikaishikinoMacBook-Air.local>
2021-11-03 09:53:56 +0900

    mecab worked on spark

--- src/preproc/create_tokenized_text.py
+++ src/preproc/create_tokenized_text.py
@@ -2,18 +2,14 @@ import sys
 sys.path.append('/tmp/work/livedoor/src')
 import os
 import pandas as pd
-import pickle5 as pickle
 from const import *
-# from sudachi_tokenizer import SudachiTokenizer
-from mecab_tokenizer import MecabTokenizer
 from datetime import datetime
 from tqdm import tqdm
-# from pyarrow.csv import read_csv, ReadOptions, ParseOptions, ConvertOptions
-# from pyarrow import int16, string
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from mecab_trial import tokenize
 
 if __name__ == '__main__':
-    # tokenizer = SudachiTokenizer()
-    tokenizer = MecabTokenizer()
     splits = ['train', 'test']
 
     for split in splits:
@@ -26,23 +22,24 @@ if __name__ == '__main__':
 
             # load
             file_i = os.path.join(DIR_DATA, f'{split}.csv')
-            # convert_options = ConvertOptions(
-            #     check_utf8=True,
-            #     column_types={'media': int16(), 'text': string()}
-            # )
-            # corpus = read_csv(file_i, convert_options=convert_options)
             corpus = pd.read_csv(file_i)#.head()
 
-            # tokenize
-            corpus['text'] = [[t for t in text] for text in tqdm(tokenizer.tokenized_corpus(corpus['text']))]
-            print('stop')  
             print(corpus.head())
+
+            # initialize spark session
+            conf = SparkConf().setMaster('local').setAppName('MyApp')
+            sc = SparkContext.getOrCreate(conf = conf)
+            sc.addFile('/tmp/work/livedoor/src/mecab_trial.py')
+
+            # tokenize
+            rdd = sc.parallelize(corpus.values)
+            rdd = rdd.map(lambda x: (x[0], [t for t in tokenize(x[1])]))
+            print(rdd.first())
+
+            # persistance
+            # rdd.saveAsTextFile('hoge.txt')
+
+            sc.stop()
             print(datetime.now(), f'{split} tokenizing done')
 
-            with open(file_o, 'wb') as f:
-                print(pickle.HIGHEST_PROTOCOL)
-                pickle.dump(corpus, f, protocol=pickle.HIGHEST_PROTOCOL)
-            # corpus.to_parquet(file_o)
-            print(datetime.now(), f'{split} file created')
-            del corpus
         print(datetime.now(), f'{split} done')
