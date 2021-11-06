d1637e7d2dd37fd261cdfe11c2f1de659e360889
五十嵐海織 <miori@igarashikaishikinoMacBook-Air.local>
2021-11-07 08:52:49 +0900

    Answer from Java side is empty; 一旦ちゃんと spark 入門した方がよさそう

--- src/preproc/create_tokenized_text.py
+++ src/preproc/create_tokenized_text.py
@@ -22,24 +22,42 @@ if __name__ == '__main__':
 
             # load
             file_i = os.path.join(DIR_DATA, f'{split}.csv')
-            corpus = pd.read_csv(file_i)#.head()
+            # corpus = pd.read_csv(file_i)#.head()
 
-            print(corpus.head())
+            # print(corpus.head())
 
             # initialize spark session
-            conf = SparkConf().setMaster('local').setAppName('MyApp')
-            sc = SparkContext.getOrCreate(conf = conf)
-            sc.addFile('/tmp/work/livedoor/src/mecab_trial.py')
+            # conf = SparkConf().setMaster('local').setAppName('MyApp')
+            # sc = SparkContext.getOrCreate(conf = conf)
+            # sc.addFile('/tmp/work/livedoor/src/mecab_trial.py')
 
             # tokenize
-            rdd = sc.parallelize(corpus.values)
-            rdd = rdd.map(lambda x: (x[0], [t for t in tokenize(x[1])]))
-            print(rdd.first())
+            # rdd = sc.parallelize(corpus.values)
+            # rdd = rdd.map(lambda x: (x[0], [t for t in tokenize(x[1])]))
+            # print(rdd.first())
 
+            # initialize spark
+            spark = SparkSession.builder \
+                .master('local') \
+                .appName('myApp') \
+                .getOrCreate()
+            spark.sparkContext.addPyFile('/tmp/work/livedoor/src/mecab_trial.py')
+            print('spark init done')
+
+            # load
+            df_corpus = spark.read.csv(file_i, header = True)
+            print(df_corpus.count())
+
+            # tokenize
+            df_corpus2 = df_corpus \
+                .rdd \
+                .map(lambda x: (x[0], [t for t in tokenize(x[1])])) \
+                .toDF(df_corpus.columns)
+            print(df_corpus2.count())
+            df_corpus2.show()
             # persistance
             # rdd.saveAsTextFile('hoge.txt')
 
-            sc.stop()
             print(datetime.now(), f'{split} tokenizing done')
 
         print(datetime.now(), f'{split} done')
