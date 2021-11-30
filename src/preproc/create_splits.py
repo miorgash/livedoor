10b79783fc5e36c68f278a5fe3ac07dd1473d6e8
五十嵐海織 <miori@igarashikaishikinoMacBook-Air.local>
2021-11-30 17:43:11 +0900

    make function

--- src/preproc/create_splits.py
+++ src/preproc/create_splits.py
@@ -1,9 +1,16 @@
 import os
+from typing import Tuple
 import pandas as pd
 from const import *
 
-if __name__ == '__main__':
-    ld_df = pd.read_csv(os.path.join(DIR_DATA, 'livedoor&text=text.csv'))
+def create_splits(input_file: str) -> Tuple:
+    '''学習用・テスト用にデータセットを分割する
+    Args:
+        input_file (str): 入力 csv ファイル
+    Returns:
+        (学習用データ, テスト用データ) のタプル
+    '''
+    ld_df = pd.read_csv(input_file)
     test = ld_df.sample(frac=0.2, random_state=123)
     train = ld_df.drop(test.index)
 
@@ -11,11 +18,17 @@ if __name__ == '__main__':
     print(test.shape)
     print(train.shape)
 
-    splits = [('train', train), ('test', test)]
-    for split, dataframe in splits:
-        file = os.path.join(DIR_DATA, f'{split}.csv')
-        if os.path.isfile(file):
-            print(f'file exists: {file}')
+    return train, test
+
+if __name__ == '__main__':
+    input_file = os.path.join(DIR_DATA, 'livedoor&text=text.csv')
+
+    train, test = create_splits(input_file)
+    
+    for split, dataframe in zip(('train', 'test'), (train, test)):
+        output_file = os.path.join(DIR_DATA, f'{split}.csv')
+        if os.path.isfile(output_file):
+            print(f'file exists: {output_file}')
         else:
-            dataframe.to_csv(file, index=False)
-            print(f'file created: {file}')
+            dataframe.to_csv(output_file, index=False)
+            print(f'file created: {output_file}')
