39ded1302be7af5d5170cf110bd9208aad004cfc
五十嵐海織 <miori@igarashikaishikinoMacBook-Air.local>
2021-11-30 17:48:18 +0900

    split worked

--- src/preproc.py
+++ src/preproc.py
@@ -1,4 +1,5 @@
 from preproc.create_table import create_table
+from preproc.create_splits import create_splits
 from const import *
 
 if __name__ == '__main__':
@@ -16,7 +17,22 @@ if __name__ == '__main__':
 
     # output
     if os.path.isfile(output_file):
-        print('file exists')
+        print(f'file exists: {output_file}')
     else:
         table.to_csv(output_file, index=False)
-        print('file created')
+        print(f'file created: {output_file}')
+    
+    # **
+    # create_splits
+    # *
+    input_file = os.path.join(DIR_DATA, 'livedoor&text=text.csv')
+
+    train, test = create_splits(input_file)
+    
+    for split, dataframe in zip(('train', 'test'), (train, test)):
+        output_file = os.path.join(DIR_DATA, f'{split}.csv')
+        if os.path.isfile(output_file):
+            print(f'file exists: {output_file}')
+        else:
+            dataframe.to_csv(output_file, index=False)
+            print(f'file created: {output_file}')
