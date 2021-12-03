6e2e0e670b48887ad4e51e2f6d1ea15497b9bf9b
五十嵐海織 <miori@igarashikaishikinoMacBook-Air.local>
2021-12-03 14:25:15 +0900

    vocab created

--- src/preproc/create_vocab.py
+++ src/preproc/create_vocab.py
@@ -1,12 +1,14 @@
-import os
 import csv
-from tokenizer.sudachi_tokenizer import SudachiTokenizer
+import os
 from collections import Counter
-from torchtext.vocab import Vocab
-from const import *
-import pickle5 as pickle
+
 import gensim
+import pickle5 as pickle
+from const import *
+from tokenizer.sudachi_tokenizer import SudachiTokenizer
+from torchtext.vocab import Vocab
 from tqdm import tqdm
+
 
 def create_vocab(input_corpus, input_pretrained_vectors) -> Vocab:
     '''学習用コーパスと学習済み分散表現に含まれる語の Vocab を作成する。
@@ -18,24 +20,27 @@ def create_vocab(input_corpus, input_pretrained_vectors) -> Vocab:
     '''
     # Vocab
     tokenizer = SudachiTokenizer()
+    counter = Counter()
+    vectors = gensim.models.KeyedVectors.load(input_pretrained_vectors)
+
+    # vectors を用いたフィルタリング
+    words_found_in_pretrained_embedding = set([t for t in vectors.vocab.keys()])
+
     with open(input_corpus, 'r') as f:
         reader = csv.reader(f)
-        header = next(reader)
-         # instanse を処理するものだけつくって map 使えばよいのでは？
+        # header
+        _ = next(reader)
+        # all text
         texts = map(lambda row: tokenizer.tokenized_text(row[1]), reader)
-        counter = Counter()
         for text in tqdm(texts):
-            counter.update(text)
-        vocab = Vocab(counter)
+            counter.update(words_found_in_pretrained_embedding & set(text))
     
-    vectors = gensim.models.KeyedVectors.load(input_pretrained_vectors)
-    
-    return vocab
+    return Vocab(counter)
 
 if __name__ == '__main__':
     input_corpus = os.path.join(DIR_DATA, 'train.csv')
     input_pretrained_vectors = '/data/chive_v1.2mc90/chive-1.2-mc90_gensim/chive-1.2-mc90.kv'
-    output_file = os.path.join(DIR_DATA, 'vocab.pkl')
+    output_file = os.path.join(DIR_BIN, 'vocab.pkl')
     vocab = create_vocab(input_corpus, input_pretrained_vectors)
 
     if os.path.isfile(output_file):
