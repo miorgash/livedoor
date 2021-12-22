2634edb08c82f9ea362d7239df3bb02526a929ad
五十嵐海織 <miori@igarashikaishikinoMacBook-Air.local>
2021-12-22 11:07:24 +0900

    Textpipeline - Send to gpu worked

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -1,26 +1,29 @@
 import os
+from typing import Tuple, Callable
 import pandas as pd
-from pandas.core.frame import DataFrame
 from torch import nn
 from torch.utils.data import DataLoader
 from const import *
 import pickle5 as pickle
 from modeling.lstm_classifier import LSTMClassifier
 from livedoor_dataset import LivedoorDataset
+from tokenizer.sudachi_tokenizer import SudachiTokenizer
+from torch.nn.utils.rnn import pad_sequence
 
-def train(model: nn.Module, labels, text) -> nn.Module:
+def train(model: nn.Module, texts: Tuple, labels: torch.tensor, text_pipeline: Callable) -> nn.Module:
     """1 バッチの学習
     Args:
         batch (): 学習用データのバッチ
     Returns:
         学習済み model
     """
-    # Tokenize & indexing
-
-
-    # Pad
+    # Tokenize, indexing and padding
+    texts = [torch.tensor(text_pipeline(text)) for text in texts]
+    texts = pad_sequence(texts, batch_first=True, padding_value=vocab['<pad>'])
 
     # Feed forward
+    labels, texts = labels.to(DEVICE), texts.to(DEVICE)
+    print(1)
 
     # Get loss
 
@@ -55,10 +58,12 @@ if __name__ == '__main__':
         h_dim = H_DIM,
         class_dim = CLASS_DIM)
     epoch = 100
+    tokenizer = SudachiTokenizer()
+    text_pipeline = lambda text: [vocab[token] for token in tokenizer.tokenized_text(text)]
 
     # 指定のエポック数だけ繰り返し
     for i in range(epoch):
         # バッチごとにループ
-        for batch, (labels, text) in enumerate(dataloader):
-            model = train(model, labels, text)
+        for batch, (labels, texts) in enumerate(dataloader):
+            model = train(model, texts, labels, text_pipeline)
         # エポックごとに保存
