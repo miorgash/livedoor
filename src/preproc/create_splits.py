5e58f47f73faf6110a56af7a89cb946b1d3da4f6
mioragsh <miorgash@gmail.com>
2022-01-03 09:48:59 +0900

    get pd.DataFrame as an arg

--- src/preproc/create_splits.py
+++ src/preproc/create_splits.py
@@ -3,30 +3,31 @@ from typing import Tuple
 import pandas as pd
 from const import *
 
-def create_splits(input_file: str) -> Tuple:
+def create_splits(ldcc_df: pd.DataFrame) -> Tuple:
     '''学習用・テスト用にデータセットを分割する
     Args:
-        input_file (str): 入力 csv ファイル
+        input_file (pd.DataFrame): 入力データ（media, {text_column}）
     Returns:
         (学習用データ, テスト用データ) のタプル
     '''
-    ld_df = pd.read_csv(input_file)
-    test = ld_df.sample(frac=0.2, random_state=123)
-    train = ld_df.drop(test.index)
+    test = ldcc_df.sample(frac=0.2, random_state=123)
+    train = ldcc_df.drop(test.index)
 
-    print(ld_df.shape)
+    print(ldcc_df.shape)
     print(test.shape)
     print(train.shape)
 
     return train, test
 
 if __name__ == '__main__':
-    input_file = os.path.join(DIR_DATA, 'livedoor&text=text.csv')
+    TEXT_COLUMN = 'title'
+    input_file = os.path.join(DIR_DATA, f'livedoor&text={TEXT_COLUMN}.csv')
+    ldcc_df = pd.read_csv(input_file)
 
-    train, test = create_splits(input_file)
+    train, test = create_splits(ldcc_df)
     
     for split, dataframe in zip(('train', 'test'), (train, test)):
-        output_file = os.path.join(DIR_DATA, f'{split}.csv')
+        output_file = os.path.join(DIR_DATA, f'{TEXT_COLUMN}.{split}.csv')
         if os.path.isfile(output_file):
             print(f'file exists: {output_file}')
         else:
