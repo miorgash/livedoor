69a1aa3b9dccf1af5627f0f344f0813585d83617
mioragsh <miorgash@gmail.com>
2022-01-03 10:47:56 +0900

    preproc_pipeline tested

--- src/preproc/preproc_pipeline.py
+++ src/preproc/preproc_pipeline.py
@@ -2,8 +2,12 @@ import pandas as pd
 from preproc.create_table import create_table
 from preproc.create_splits import create_splits
 from preproc.create_vocab import create_vocab
+from preproc.create_vectors import create_vectors
 from const import *
 from util import create_dataframe_if_not_exists, create_pickle_if_not_exists
+import gensim
+import pickle5 as pickle
+
 
 if __name__ == '__main__':
 
@@ -29,11 +33,20 @@ if __name__ == '__main__':
         )
     
     print("# create_vocab")
-    input_corpus = os.path.join(DIR_DATA, '{TEXT_COLUMN}.train.csv')
+    input_corpus = os.path.join(DIR_DATA, f'{TEXT_COLUMN}.train.csv')
     input_pretrained_vectors = '/data/chive_v1.2mc90/chive-1.2-mc90_gensim/chive-1.2-mc90.kv'
-    output_file = os.path.join(DIR_BIN, '{TEXT_COLUMN}.vocab.pkl')
+    output_file = os.path.join(DIR_BIN, f'{TEXT_COLUMN}.vocab.pkl')
     vocab = create_vocab(input_corpus, input_pretrained_vectors)
     create_pickle_if_not_exists(vocab, output_file)
 
     print("# create_vectors")
-    
+    with open(os.path.join(DIR_BIN, 'title.vocab.pkl'), 'rb') as f:
+        vocab = pickle.load(f)
+    pretrained_vectors = gensim.models.KeyedVectors.load(
+        '/data/chive_v1.2mc90/chive-1.2-mc90_gensim/chive-1.2-mc90.kv'
+    )
+    vectors = create_vectors(vocab, pretrained_vectors)
+    create_pickle_if_not_exists(
+        pickle_obj=vectors,
+        output_file=os.path.join(DIR_BIN, 'title.vectors.pkl')
+    )
