951ce33c54cb67f55758865e498e51acb26f78d7
mioragsh <miorgash@gmail.com>
2022-01-17 07:18:05 +0900

    remove utils not in use

--- src/const.py
+++ src/const.py
@@ -7,7 +7,5 @@ DIR_DATA = os.path.join(ROOT, 'data')
 DIR_MODEL = os.path.join(ROOT, 'model')
 DIR_LOG = os.path.join(ROOT, 'log')
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-TOKENIZER = 'mecab' # 'mecab' or 'sudachi'
 # !echo $(mecab-config --dicdir)/mecab-ipadic-neologd の出力結果
 DIR_MECAB_DIC = '/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd'
-SAMPLE_SENT = 'ワンマンライブに行きたい。'
